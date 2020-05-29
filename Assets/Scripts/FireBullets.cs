7acc4720a1a613e9b13f1a76204ade6a2027d651
Antoine <zlino.dev@gmail.com>
2020-05-29 14:21:40 +0200

    Project added

---
+++ Assets/Scripts/FireBullets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireBullets : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public int shootForce = 2000;
+    public Text numberOfShootText;
+    private int numberOfShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            numberOfShoot++;
+            numberOfShootText.text = "" + numberOfShoot;
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+        bulletObject.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+    }
+}
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    static int numberCubeLeft;
+    private Text numberCubeLeftText;
+    private bool asTouchTheGround = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Ground" && !asTouchTheGround)
+        {
+            asTouchTheGround = true;
+            numberCubeLeft--;
+            numberCubeLeftText.text = "" + numberCubeLeft;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberCubeLeftText = GameObject.Find("NumberCubeLeft").GetComponent<Text>();
+        numberCubeLeft++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(numberCubeLeft == 0)
+        {
+            GameObject.Find("Victory").GetComponent<Text>().enabled = true;
+            Time.timeScale = 0;
+        }
+    }
+}
